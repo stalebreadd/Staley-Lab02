41d219b612529b19414f48f464afc86a53131014
unknown <14268@HS222-05.vtsd.com>
2015-09-22 11:54:28 -0400

    hello

---
+++ Lab02C.java
@@ -0,0 +1,25 @@
+
+/**
+Shane Staley
+100 Point version output
+ */
+public class Lab02C
+{
+    public static void main(String args[])
+    {
+        System.out.println("Lab 02C, 100 Point Version");
+        System.out.println();
+        System.out.println("First Java Program");
+        System.out.println();
+        System.out.println("Measure Today's knowledge by yesterday's confusion");
+        System.out.println();
+        System.out.println("Bewilderment  +  Exposure  =  Obvious");
+        System.out.println();
+        System.out.println("# ##### #####   ##### ##### # #    # ##### #####");
+        System.out.println("# #   # #   #   #   # #   # # ##   #   #   #    ");
+        System.out.println("# #   # #   #   ##### #   # # # #  #   #   #####");
+        System.out.println("# #   # #   #   #     #   # # #   ##   #       #");
+        System.out.println("# ##### #####   #     ##### # #    #   #   #####");
+    }
+}
+        
